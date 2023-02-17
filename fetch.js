70ff13ed18d98322c1d23aa9eeca7f68b81af721
Juli <laterzajulian@gmail.com>
2023-02-17 20:04:41 -0300

    fetch terminado

--- fetch.js
+++ fetch.js
@@ -1,5 +1,34 @@
-const url = 'https://jsonplaceholder.typicode.com/users';
+const url = 'http://jsonplaceholder.typicode.com/users'
 
-fetch(url)
-    .then((response) => response.json())
-    .then(data => console.log(data))
+
+// Esta funcion recibe 2 parámetros, el primero es la URL y el segundo una función.
+// fetch(url, {
+//     method: 'POST', //También existe GET, POST, PUT, PATCH, DELETE
+//     headers: { //En el header está la información de que es lo que vamos a cargar
+//         'Content-Type': 'application/json',
+//         'Authorization': 'Bearer acasedebeingresarElTokenDeAutorizacion'
+//     },
+//     body: JSON.stringify({ //Toda la información de JSON a String
+//         name: 'Juli',
+//         website: 'google.com'
+//     })
+// }).then((response) => response.json()).then(data => console.log(data))  // Se le debe agregar la función then(data => console.log(data) para visualizar
+
+
+const fn = async () => {
+    const response = await fetch(url, {
+        method: 'POST', //También existe GET, POST, PUT, PATCH, DELETE SIRVE PARA CREAR CONEXIÓN CON UNA API REST.
+        headers: { //En el header está la información de que es lo que vamos a cargar
+            'Content-Type': 'application/json', // Es para que el servidor entienda QUE TIPO de dato le estamos enviando. En este caso es un JSON
+            'Authorization': 'Bearer acasedebeingresarElTokenDeAutorizacion' //Acá se envía el token de autorización
+        },
+        body: JSON.stringify({ //Toda la información  que le envío al SERVIDOR. de JSON a String
+            name: 'Juli',
+            website: 'google.com'
+        })
+    })
+    const data = await response.json()
+    console.log(data)
+}
+
+fn()
