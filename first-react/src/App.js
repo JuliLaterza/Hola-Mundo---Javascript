98f228ec894b24811852f20e2a0a7f9608bd7228
Juli <laterzajulian@gmail.com>
2023-03-06 01:59:22 -0300

    solucion de react y web

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   return (
+    /*
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +20,12 @@ function App() {
         </a>
       </header>
     </div>
+    */
+   <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+   </ul>
   );
 }
 
--- first-react/src/index.js
+++ first-react/src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
-//root.render(
-//  <React.StrictMode>
- //   <App />
-//  </React.StrictMode>
-//);
-
-const x = <h1>Hello World</h1>;
-ReactDOM.render(x, document.getElementById('root'));
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
