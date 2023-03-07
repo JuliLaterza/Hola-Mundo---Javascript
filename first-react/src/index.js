8e2b541899b4ea98b45f34c0e8611e95fddd68c3
Juli <laterzajulian@gmail.com>
2023-03-07 20:20:06 -0300

    creacion de componentes ul

--- first-react/src/index.js
+++ first-react/src/index.js
@@ -1,15 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
+
+const Li = ({ children, estado }) => {
+  console.log(children, estado)
+  return (
+    <li> {children} es {estado} </li>
+  )
+}
+
+const X = () =>
+  <ul>
+    <Li
+      children={"Pepe"}
+      estado={"Feliz"}    
+    ></Li>
+    <Li
+      children={"Juan"}
+      estado={"Triste"}
+    >
+    </Li>
+    <Li
+      children={"Maria"}
+      estado={"Enojada"}
+    >
+
+
+    </Li>
+  </ul>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<X />);
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
