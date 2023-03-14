21eac5dbc8b0766a4254b66036576663ef56071a
Juli <laterzajulian@gmail.com>
2023-03-14 20:55:10 -0300

    Creacion de componentes Button y App

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,21 +1,56 @@
-import Button from "./Button"
+import { Component } from "react" //Obligatorio
 
-const App = () => {
-    const miVariable = true
-
-    if (miVariable){
-        return (
-            <p>Mi variable es true</p>
-        )
+class Button extends Component {
+    constructor(props) {
+        super(props) //Super hace referencia al componente de Components
+        console.log('Constructor',props)
     }
 
-    return (
-        <div>
-            <h1 onClick={(e) => console.log('clic',e)}>Hola mundo</h1>
-            <Button onClick={() => console.log('Se le hizo click')} >Enviar</Button>
-            
-        </div>
-    )
+    componentDidMount() {
+        console.log('ComponentDidMount')
+    }
+
+    componentDidUpdate(prevProps,prevState) {
+        console.log('componentDidUpdate',prevProps,prevState)
+    } //Solo se ejecuta cuando actualizamos un estado.
+
+    componentWillUnmount() {
+        console.log('desmontando el componente', this.props,this.state)
+    }
+
+    render () {
+        console.log('Ejecutando método render de Button')
+        return (
+            <button onClick={()=> this.setState({ prop:1 })}>
+                Enviar 2.0
+            </button>
+        )
+    }
 }
+
+
+
+class App extends Component {
+
+    state = {
+        valor:3
+    }
+    render() {
+        console.log(this.state)
+        return (
+            <div>
+                <p>Hola Mundo!</p>
+                {this.state.valor ===3 ?<Button chanchito='feliz'/>: null}
+                <button
+                className={`${this.state.valor}`} 
+                onClick={()=> this.setState({ valor:2 })} >
+                    Enviar
+                </button>
+            </div>
+        )
+    }
+}
+
+
 
 export default App
