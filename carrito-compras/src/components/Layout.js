e046bbdcb95d29a87db61dc163b8c96c551452a6
Juli <laterzajulian@gmail.com>
2023-03-19 00:34:38 -0300

    Layout component

---
+++ carrito-compras/src/components/Layout.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+
+const styles = {
+    layout: {
+        backgroundColor: '#fff',
+        color: '#0A283E',
+        display: 'flex',
+        alingItems: 'center',
+        justifyContent: 'center',
+    },
+    container: {
+        width: '80%',
+    }
+}
+
+class Layout extends Component {
+    render() {
+        return (
+            <div style={styles.layout}>
+                <div style={styles.container}>
+                    {this.props.children}
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Layout
