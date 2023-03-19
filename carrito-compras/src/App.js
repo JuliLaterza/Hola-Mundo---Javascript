0c496dd3c498c3c820fd37290de54805713f9dbc
Juli <laterzajulian@gmail.com>
2023-03-19 00:35:14 -0300

    App.js Layout + Productos

--- carrito-compras/src/App.js
+++ carrito-compras/src/App.js
@@ -1,23 +1,26 @@
 import { Component } from 'react';
 import Productos from './components/Productos';
+import Layout from './components/Layout';
 
 class App extends Component { 
 
   state = {
     productos: [
-      {name: 'tomate', price: 1000, img: '/productos/tomate.jpg'},
-      {name: 'lechuga', price: 2000, img: '/productos/lechuga.jpg'},
-      {name: 'arbeja', price: 150, img: '/productos/arbejas.jpg'},
+      {name: 'Tomate', price: 1000, img: '/productos/tomate.jpg'},
+      {name: 'Lechuga', price: 2000, img: '/productos/lechuga.jpg'},
+      {name: 'Arbeja', price: 150, img: '/productos/arbejas.jpg'},
     ]
   }
 
   render(){
     return (
       <div>
-        <Productos
-        agregarAlCarrito={()=> console.log('agregando al carrito')}
-        productos={this.state.productos}
-        />
+        <Layout>
+          <Productos
+          agregarAlCarrito={()=> console.log('agregando al carrito')}
+          productos={this.state.productos}
+          />
+        </Layout>
       </div>
     )
   }
