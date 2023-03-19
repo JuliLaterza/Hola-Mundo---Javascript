cfdfc610bb361f1f79650c1ed93fd1a5354759c4
Juli <laterzajulian@gmail.com>
2023-03-19 01:39:24 -0300

    Carro component

---
+++ carrito-compras/src/components/Carro.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import BubbleAlert from "./BubbleAlert";
+
+const styles = {
+    carro: {
+        backgroundColor: '#52cf46',
+        color: '#fff',
+        border: 'none',
+        borderRadius: '15px',
+        cursor: 'pointer',
+        padding: '10px 15px',
+    },
+    bubble: {
+        position: 'relative',
+        left: 12,
+        top: 20,
+    }
+}
+
+
+class Carro extends Component {
+    render() {
+        return (
+            <div>
+                <span style={styles.bubble} >
+                    <BubbleAlert value = {10} />
+                </span>
+                <button style={styles.carro}>
+                    Carro
+                </button>
+            </div>
+        )
+    }
+}
+
+export default Carro
